0f33ff75253077d690536456e606e48cc0bd668e
JojoDom <adwoa_dadson@yahoo.com>
2023-04-16 12:36:32 +0000

    Initial Commit

---
+++ Shopping-UI-ios/Shopping_UI_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_UI_iosApp.swift
+//  Shopping-UI-ios
+//
+//  Created by Adwoa Dadson on 16/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_UI_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
